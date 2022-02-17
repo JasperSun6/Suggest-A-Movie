4bea48a3e6b5175cc43f5ffabf3f191ef0765e7c
sun00120 <sun00120@algonquinlive.com>
2022-02-17 15:42:40 -0500

    Modified html files, finish openDatabase, check if the input is valid and if the result in the database.

---
+++ js/app.js
@@ -0,0 +1,221 @@
+const APP = {
+  DB: null, //the indexedDB
+  isONLINE: "onLine" in navigator && navigator.onLine,
+  tmdbBASEURL: "https://api.themoviedb.org/3/",
+  tmdbAPIKEY: "527917a705e7338ceca3903f95d79899",
+  tmdbIMAGEBASEURL: "http://image.tmdb.org/t/p/w500",
+  input: "",
+  results: [],
+  init: () => {
+    //when the page loads
+    //open the database
+    APP.openDatabase(APP.registerSW); //register the service worker after the DB is open
+  },
+
+  registerSW: () => {
+    //register the service worker
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(function (error) {
+        // Something went wrong during registration. The sw.js file
+        // might be unavailable or contain a syntax error.
+        console.warn(error);
+      });
+      navigator.serviceWorker.ready.then((registration) => {
+        // .ready will never reject... just wait indefinitely
+        registration.active;
+        //save the reference to use later or use .ready again
+      });
+    }
+    //then add listeners and run page specific code
+    APP.pageSpecific();
+    APP.addListeners();
+  },
+
+  openDatabase: (nextStep) => {
+    //open the database
+    let version = 1;
+
+    let dbOpenRequest = indexedDB.open("movieDB", version);
+    //add the update, error, success listeners in here
+    dbOpenRequest.onupgradeneeded = function (ev) {
+      APP.DB = ev.target.result;
+
+      try {
+        APP.DB.deleteObjectStore("searchResults");
+        APP.DB.deleteObjectStore("suggestResults");
+      } catch {
+        console.log("Can't delete the database.");
+      }
+
+      //create searchStore with keyword as keyPath
+      APP.DB.createObjectStore("searchResults", {
+        keyPath: "keyword",
+        autoIncrement: false,
+      });
+
+      //create suggestStore with movieid as keyPath
+      APP.DB.createObjectStore("suggestResults", {
+        keyPath: "movieid",
+        autoIncrement: false,
+      });
+    };
+    //call nextStep onsuccess
+    dbOpenRequest.onsuccess = function (ev) {
+      APP.DB = dbOpenRequest.result;
+      console.log(`${APP.DB.name} is ready to be used!`);
+    };
+
+    nextStep();
+  },
+
+  createTransaction: (storeName) => {
+    let tx;
+    //create a transaction to use for some interaction with the database
+    tx = APP.DB.transaction(storeName, "readWrite");
+    return tx;
+  },
+
+  getDBResults: (storeName, keyValue) => {
+    //return the results from storeName where it matches keyValue
+  },
+
+  addResultsToDB: (obj, storeName) => {
+    //pass in the name of the store
+    //save the obj passed in to the appropriate store
+  },
+
+  addListeners: () => {
+    //add listeners
+    let btnSearch = document.getElementById("btnSearch");
+    btnSearch.addEventListener("click", APP.searchFormSubmitted());
+    //when the search form is submitted
+    //when clicking on the list of possible searches on home or 404 page
+    //when a message is received
+    //when online and offline
+
+    window.addEventListener("online", APP.changeStatus);
+    window.addEventListener("offline", APP.changeStatus);
+  },
+
+  pageSpecific: () => {
+    //anything that happens specifically on each page
+    if (document.body.id === "home") {
+      //on the home page
+    }
+    if (document.body.id === "results") {
+      //on the results page
+      //listener for clicking on the movie card container
+    }
+    if (document.body.id === "suggest") {
+      //on the suggest page
+      //listener for clicking on the movie card container
+    }
+    if (document.body.id === "fourohfour") {
+      //on the 404 page
+    }
+  },
+
+  changeOnlineStatus: (ev) => {
+    //when the browser goes online or offline
+  },
+
+  messageReceived: (ev) => {
+    //ev.data
+  },
+
+  sendMessage: (msg) => {
+    //send a message to the service worker
+  },
+
+  searchFormSubmitted: (ev) => {
+    ev.preventDefault();
+    //get the keyword from teh input
+    APP.input = document.getElementById("search").value;
+    console.log(input);
+
+    //make sure it is not empty
+    //check the db for matches
+    //do a fetch call for search results
+    //save results to db
+    //navigate to url
+    if (!input) {
+      alert("Empty input, please try it again.");
+    } else {
+      let searchResult = APP.createTransaction("searchResults");
+      let searchStore = searchResult.objectStore("searchResults");
+      let getResult = searchStore.get(APP.input);
+      getResult.onsuccess = (ev) => {
+        if (ev.target.result === undefined) {
+          APP.getData(APP.input);
+        } else {
+          console.log("Results already in the Database");
+        }
+      };
+    }
+  },
+
+  cardListClicked: (ev) => {
+    // user clicked on a movie card
+    //get the title and movie id
+    //check the db for matches
+    //do a fetch for the suggest results
+    //save results to db
+    //build a url
+    //navigate to the suggest page
+  },
+
+  getData: (endpoint, callback) => {
+    //do a fetch call to the endpoint
+
+    let url = `${APP.tmdbBASEURL}search/movie?api_key=${APP.tmdbAPIKEY}&query=${endpoint}`;
+
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 400) {
+          throw new NetworkError(
+            `Failed fetch to ${url}`,
+            resp.status,
+            resp.statusText
+          );
+        }
+        return resp.json();
+      })
+      .then((contents) => {
+        let results = contents.results;
+        //remove the properties we don't need
+        //save the updated results to APP.results
+        // call the callback
+      })
+      .catch((err) => {
+        //handle the NetworkError
+        alert(`Error: ${err.name} ${err.message}`);
+      });
+  },
+
+  getSearchResults: (keyword) => {
+    //check if online
+    //check in DB for match of keyword in searchStore
+    //if no match in DB do a fetch
+    // APP.displayCards is the callback
+  },
+
+  getSuggestedResults: (movieid) => {
+    //check if online
+    //check in DB for match of movieid in suggestStore
+    //if no match in DB do a fetch
+    // APP.displayCards is the callback
+  },
+
+  displayCards: () => {
+    //display all the movie cards based on the results array
+    // in APP.results
+    //these results could be from the database or from a fetch
+  },
+
+  navigate: (url) => {
+    //change the current page
+    window.location = url; //this should include the querystring
+  },
+};
+
+document.addEventListener("DOMContentLoaded", APP.init);
---
+++ sw.js
@@ -0,0 +1,116 @@
+const version = 1;
+let isOnline = true;
+const staticCache = `pwaAssignStaticCache${version}`;
+const dynamicCache = `pwaAssignDynamicCache${version}`;
+const cacheList = [
+  "/",
+  "/index.html",
+  "/results.html",
+  "/suggest.html",
+  "/404.html",
+  "/manifest.json",
+  "/favicon.ico",
+  "./css/main.css",
+  "./js/app.js",
+  "./img/imageNotFound.png",
+  "./img/android-chrome-192x192.png",
+  "./img/android-chrome-512x512.png",
+  "./img/apple-touch-icon.png",
+  "./img/favicon-16x16.png",
+  "./img/favicon-32x32.png",
+  "./img/mstile-150x150.png",
+  "https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap",
+];
+
+self.addEventListener("install", (ev) => {
+  ev.waitUntil(
+    caches.open(staticCache).then((cache) => {
+      cache.addAll(cacheList);
+    })
+  );
+});
+
+self.addEventListener("activate", (ev) => {
+  ev.waitUntil(
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => {
+              if (key === staticCache || key === dynamicCache) {
+                return false;
+              } else {
+                return true;
+              }
+            })
+            .map((key) => caches.delete(key))
+        );
+      })
+      .catch(console.warn)
+  );
+});
+
+self.addEventListener("fetch", (ev) => {
+  ev.respondWith(
+    caches.match(ev.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(ev.request)
+          .then((fetchRes) => {
+            if (!fetchRes.ok) throw new Error(fetchRes.staticText);
+            return caches.open(dynamicCache).then((cache) => {
+              let copy = fetchRes.clone();
+              cache.put(ev.request, copy);
+              return fetchRes;
+            });
+          })
+          .catch((err) => {
+            console.log("SW fetch failed");
+            console.warn(err);
+            if (ev.request.mode == "navigate") {
+              return caches.match("/404.html").then((cacheRes) => {
+                return cacheRes;
+              });
+            }
+          })
+      );
+    })
+  );
+});
+
+self.addEventListener("message", (ev) => {
+  console.log(ev.data);
+  if (ev.data.ONLINE) {
+    isOnline = ev.data.ONLINE;
+  }
+});
+
+function sendMessage(msg) {
+  self.clients.matchAll().then(function (clients) {
+    if (clients && clients.length) {
+      clients[0].postMessage(msg);
+    }
+  });
+}
+
+function limitCache() {
+  //remove some files from the dynamic cache
+  const limitCacheSize = (nm, size = 25) => {
+    caches.open(nm).then((cache) => {
+      cache.keys().then((keys) => {
+        if (keys.length > size) {
+          cache.delete(keys[0]).then(() => {
+            limitCacheSize(nm, size);
+          });
+        }
+      });
+    });
+  };
+}
+
+function checkForConnection() {
+  //try to talk to a server and do a fetch() with HEAD method.
+  //to see if we are really online or offline
+  //send a message back to the browser
+}
